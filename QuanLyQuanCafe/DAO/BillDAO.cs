bfa994d1770a880003d1cb89ed48b725d69ec4df
hoangphuc06 <19520222@gm.uit.edu.vn>
2020-12-02 15:13:16 +0700

    add checkout code and bill class

---
+++ QuanLyQuanCafe/DAO/BillDAO.cs
@@ -0,0 +1,70 @@
+﻿using QuanLyQuanCafe.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class BillDAO
+    {
+        private static BillDAO instance;
+
+
+        public static BillDAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BillDAO();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+
+        private BillDAO() { }
+
+        public int GetUncheckBillIDByTableID(int id)
+        {
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM Bill WHERE ID_TableFood = " + id.ToString() + " AND status = 0");
+            if (data.Rows.Count > 0)
+            {
+                Bill bill = new Bill(data.Rows[0]);
+                return bill.ID;
+            }
+            return -1;
+            // Thành công: bill ID
+            // Thất bại: -1 
+        }
+
+        public void CheckOut(int id, int discount, float totalPrice)
+        {
+            string query = "Update dbo.Bill SET DateCheckOut = GETDATE(), status = 1, " + "discount = " + discount + ", totalPrice = " + totalPrice + " WHERE ID_Bill = " + id;
+            DataProvider.Instance.ExecuteNonQuery(query);
+        }
+        public void InsertBill(int id)
+        {
+            DataProvider.Instance.ExecuteNonQuery("exec USP_InsertBill @idTable", new object[] { id });
+
+        }
+        public DataTable GetBillListByDate(DateTime checkIn, DateTime checkOut)
+        {
+            return DataProvider.Instance.ExecuteQuery("exec USP_GetListBillByDate @checkIn , @checkOut", new object[] { checkIn, checkOut });
+        }
+        public int GetMaxIdBill()
+        {
+            try
+            {
+                return (int)DataProvider.Instance.ExecuteScalar("SELECT MAX(ID_Bill) FROM dbo.Bill");
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ QuanLyQuanCafe/DTO/Bill.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Bill
+    {
+        private int iD;
+        private int status;
+        private int discount;
+        private DateTime? dateCheckIn;
+        private DateTime? dateCheckOut;
+        public int ID { get => iD; set => iD = value; }
+        public DateTime? DateCheckIn { get => dateCheckIn; set => dateCheckIn = value; }
+        public DateTime? DateCheckOut { get => dateCheckOut; set => dateCheckOut = value; }
+        public int Status { get => status; set => status = value; }
+        public int Discount { get => discount; set => discount = value; }
+
+        public Bill(int id, DateTime? dateCheckIn, DateTime? dateCheckOut, int status, int discount)
+        {
+            this.ID = id;
+            this.DateCheckIn = dateCheckIn;
+            this.DateCheckOut = dateCheckOut;
+            this.Status = status;
+            this.Discount = discount;
+        }
+
+        public Bill(DataRow row)
+        {
+            this.ID = (int)row["ID_Bill"];
+            this.DateCheckIn = (DateTime?)row["DateCheckIn"];
+
+            var DateCheckOutTemp = row["DateCheckOut"];
+            if (DateCheckOutTemp.ToString() != "")
+                this.DateCheckOut = (DateTime?)DateCheckOutTemp;
+
+            this.Status = (int)row["status"];
+
+            if (row["discount"].ToString() != "")
+
+                this.Discount = (int)row["discount"];
+        }
+
+    }
+}
