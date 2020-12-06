f3b12be580035d6105a455386c74c3595b36b736
Your Name <19521588@gm.uit.edu.vn>
2020-12-06 12:09:18 +0700

    DeleteBill

--- QuanLyQuanCafe/fCheckOut.cs
+++ QuanLyQuanCafe/fCheckOut.cs
@@ -42,72 +42,23 @@ namespace QuanLyQuanCafe
             lbTotalPrice.Text = TotalPrice.ToString("c", culture);
 
         }
-        private void btnAdd500k_Click(object sender, EventArgs e)
+      
+
+        
+
+
+        void load()
         {
-            SumPrice += 500000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
+            CultureInfo culture = new CultureInfo("vi-VN");
+            lbPayPrice.Text = PayPrice.ToString("c", culture);
+            lbSumMoney.Text = SumPrice.ToString("c", culture);
+            lbExcessMoney.Text = ExcessPrice.ToString("c", culture);
         }
 
-        private void btnAdd200k_Click(object sender, EventArgs e)
+        private void nmDiscount_ValueChanged(object sender, EventArgs e)
         {
-            SumPrice += 200000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd100k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 100000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd50k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 50000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd20k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 20000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd10k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 10000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd5k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 5000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd2k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 2000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAdd1k_Click(object sender, EventArgs e)
-        {
-            SumPrice += 1000;
-            ExcessPrice = SumPrice - PayPrice;
-            load();
-        }
-
-        private void btnAddFullMoney_Click(object sender, EventArgs e)
-        {
-            SumPrice = PayPrice;
+            int discount = Convert.ToInt32(nmDiscount.Value);
+            PayPrice = TotalPrice - (TotalPrice * discount) / 100;
             ExcessPrice = SumPrice - PayPrice;
             load();
         }
@@ -130,39 +81,89 @@ namespace QuanLyQuanCafe
                         this.Close();
                     }
                 }
-            }    
+            }
             else
             {
                 MessageBox.Show("Số tiền thanh toán chưa đủ !");
-            }    
-            
+            }
+
         }
 
-        private void btnReset_Click(object sender, EventArgs e)
+        private void btnAddFullMoney_Click(object sender, EventArgs e)
         {
-            nmDiscount.Value = 0;
-            PayPrice = TotalPrice;
-            SumPrice = 0;
-            ExcessPrice = 0;
-            load();
-        }
-
-        void load()
-        {
-            CultureInfo culture = new CultureInfo("vi-VN");
-            lbPayPrice.Text = PayPrice.ToString("c", culture);
-            lbSumMoney.Text = SumPrice.ToString("c", culture);
-            lbExcessMoney.Text = ExcessPrice.ToString("c", culture);
-        }
-
-        private void nmDiscount_ValueChanged(object sender, EventArgs e)
-        {
-            int discount = Convert.ToInt32(nmDiscount.Value);
-            PayPrice = TotalPrice - (TotalPrice * discount) / 100;
+            SumPrice = PayPrice;
             ExcessPrice = SumPrice - PayPrice;
             load();
         }
 
-  
+
+
+        private void btnAdd1k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 1000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd2k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 2000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd5k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 5000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd10k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 10000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd20k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 20000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd50k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 50000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd100k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 100000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd200k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 200000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+        private void btnAdd500k_Click_1(object sender, EventArgs e)
+        {
+            SumPrice += 500000;
+            ExcessPrice = SumPrice - PayPrice;
+            load();
+        }
+
+       
+       
+
+        
     }
 }
